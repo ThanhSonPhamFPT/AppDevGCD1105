7da657315222f4ad8e96f83f9df6ea92adf1cb68
Pham Thanh Son <sonpt28@fpt.edu.vn>
2024-07-27 09:15:25 +0700

    List all category

--- WebApp/Controllers/CategoryController.cs
+++ WebApp/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> myList = _dbContext.Categories.ToList();
+            return View(myList);
         }
     }
 }
